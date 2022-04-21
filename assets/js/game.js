61c94d3e172a61cb5d9c9bd47006f3ca7da79bf1
Amanda D. Babineaux <adbabineaux@southtexascollege.edu>
2022-04-21 13:36:12 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -27,14 +27,17 @@ var fight = function(enemyName) {
             if (confirmSkip) {
                 window.alert(playerName + ' has decided to skip this fight. Goodbye!');
                 // subtract money from playerMoney for skipping
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney", playerMoney);
                 break;
             }
         }
 
+        // generate random damage value based on player's attack power
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+
         // remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
+        enemyHealth = Math.max(0, enemyHealth - damage);
         console.log(playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.');
 
         // check enemy's health
@@ -50,8 +53,10 @@ var fight = function(enemyName) {
             window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
         }
 
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
         // remove players's health by subtracting the amount set in the enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
+        playerHealth = Math.max(0, playerHealth - damage);
         console.log(enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.');
 
         // check player's health
@@ -83,7 +88,7 @@ var startGame = function() {
             var pickedEnemyName = enemyNames[i];
 
             // reset enemyHealth before starting new fight
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
 
             // use debugger to pause script from running and check what's going on at that moment in the code
             // debugger;
@@ -117,7 +122,7 @@ var startGame = function() {
 var endGame = function() {
     // if player is still alive, player wins!
     if (playerHealth > 0){
-        window.alert("Great job, you survived the game! You now have a score of " + playermoney + ".");
+        window.alert("Great job, you survived the game! You now have a score of " + playerMoney + ".");
     } else {
         window.alert("You've lost your robot in battle.");
     }
@@ -183,5 +188,11 @@ var shop = function(){
     }
 }
 
+// function to generate a random numeric value
+var randomNumber = function(min, max){
+    var value = Math.floor(Math.random() * max - min + 1) + min;
+
+    return value;
+}
 // start the game when the page loads
 startGame();
